777ca78a78a6a6dbb9f354f94a874b59b9c4ecd0
Jacek Debski <jacek.debski@hotmail.com>
2023-01-27 21:48:13 +0100

    2. formatowanie kodu

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,16 +2,21 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
-int totalRejected = 0;
+	int totalRejected = 0;
 
 	public int reportRejected() {
 		return totalRejected;
 	}
 
 	public void countIn(int in) {
-	if (!callCheck() && in > peekaboo())
+		if (!callCheck() && in > peekaboo())
 			totalRejected++;
-			else
-				super.countIn(in);
+		else
+			super.countIn(in);
 	}
 }
+//Pytanie: Które wiersze w klasie HanoiRhymer były źle sformatowane?
+//		W klasie HanoiRhymer źle sformatowane były wiersze: 5, 12, 14 i 15.
+//Zweryfikuj działanie kombinacji klawiszy alt + ← oraz alt + →.
+//Skrót umożliwia przechodzenie pomiędzy ostatnio modyfikowanymi linijkami w projekcie, gdy wczytana jest opcja
+//Keymap:Eclipse w IntelliJ IDEA.
